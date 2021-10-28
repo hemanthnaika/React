dcc57952856dbbdd5d20f214c19c952be8329963
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-28 13:34:16 +0530

    Create React app

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, eaque reiciendis consectetur, maxime beatae sequi doloremque asperiores odit eligendi adipisci eius? Minus ratione in odio aperiam optio vitae perferendis! Ducimus?
+      <h4>Im h4</h4>
+      <h4>Im h4</h4>
+      <h4>Im h4</h4>
+      <h4>Im h4</h4>
+    </div>
+  );
+}
+
+export default App;
