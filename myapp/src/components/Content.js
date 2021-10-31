fcdf47cab115821f43841c3cf45fae195553c594
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-31 17:01:56 +0530

    Lifecule in React

--- myapp/src/components/Content.js
+++ myapp/src/components/Content.js
@@ -2,11 +2,14 @@ import ListItem from "./Listitem";
 
 const Content = () => {
   const chores = ["item 1", "Item 2", "Item 3", "Item 4"];
+  console.log("I Mounted");
   return (
     <div className="container">
       <ul>
+        
         {chores.map((chore) => (
-          <ListItem chore={chore}/>
+          // Update
+          <ListItem chore="Hi" />
         ))}
       </ul>
     </div>
