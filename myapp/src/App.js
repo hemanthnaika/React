233a9f11f4d8e2d8107920fbcfadd8aff90a7782
hemanthnaika <hemanthnaika22@gmail.com>
2021-11-13 16:25:57 +0530

    Redux-state

---
+++ myapp/src/App.js
@@ -0,0 +1,11 @@
+import Two from "./components/Two";
+import reducer from "./reducer/reducer";
+
+const App = () => {
+  return ( <div>
+    <Two/>
+<reducer/>
+  </div> );
+}
+ 
+export default App;
---
+++ myapp/src/reducer/reducer.js
@@ -0,0 +1,11 @@
+const reducer = (state = {}, action) => {
+  switch (action.type) {
+      case "ADD_PRODUCT":
+          console.log(action)
+          return state
+      default:
+          return state
+  }
+}
+
+export default reducer
