3a493b0b638c131bc1058880d1e7e130f0f77e25
Hemanth <90907017+hemanthnaika@users.noreply.github.com>
2021-11-14 20:37:56 +0530

    To creact Navbar and change the Router

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import Products from './components/Products'
 import Details from './components/Details'
+// ! To creat Router using React Dom
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './layout/Navbar';
 
 
 const App = () => {
-
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Products />} />
         <Route path='/details' element={<Details />} />
